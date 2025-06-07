c5dbcf53e46b4ff7069f4368025790e0e8db61d2
Bruno M Belo <bmbelo87@gmail.com>
2025-06-06 23:43:10 -0300

    Change the default theme

--- Themes/default/Graphics/ScreenSelectMusic SortOrderFrame.lua
+++
@@ -1,8 +0,0 @@
-return Def.ActorFrame {
-	LoadActor(THEME:GetPathG("OptionRowExit","frame")) .. {
-		InitCommand=cmd(diffusebottomedge,Color("Orange"));
-	};
-	LoadActor(THEME:GetPathG("_icon","Sort")) .. {
-		InitCommand=cmd(x,-60;shadowlength,1;);
-	};
-};
--- Themes/default/Graphics/ScreenWithMenuElements Footer.lua
+++
@@ -1,7 +0,0 @@
-local t = Def.ActorFrame {};
-
-t[#t+1] = Def.Quad {
-	InitCommand=cmd(vertalign,bottom;zoomto,SCREEN_WIDTH+1,34;diffuse,Color.Black);
-};
-
-return t;
